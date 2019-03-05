4e0e97bc946fe936c592ce1a914c053f377b8d73
hgonomeg <hgonomeg@gmail.com>
2019-03-05 19:20:09 +0100

    Poprawka

--- Grawitacja.cpp
+++ Grawitacja.cpp
@@ -8,7 +8,7 @@ int main(int argc, char** argv)
 	sf::RenderWindow rehn(sf::VideoMode(500,500),"REHN");
 	rehn.setFramerateLimit(60);
 	
-	sim.add_body(new Celestial_body(200));
+	//sim.add_body(new Celestial_body(200));
 	
 	while(rehn.isOpen())
 	{
