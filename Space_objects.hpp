3c54915da45ccc4b7c5ff9ca3ae3ccf1d9228ac0
PanZWarzywniaka <tupolewcx@gmail.com>
2019-03-07 18:50:52 +0100

    Propozycje dalszych zmian czytajkomentarze w kodzie

--- Space_objects.hpp
+++ Space_objects.hpp
@@ -2,6 +2,9 @@
 
 class Planet :public Celestial_body
 {
+	std::string state_of_matter; // moja propozycja; w przyszłości w zależności od stanu skupienia będzie mozna wybrać odpowiednią teksturę/kolor
+
+
 	public:
 	
 	Planet(unsigned int mass=10, sf::Color& planet_color=sf::Color::Blue, sf::Vector2f& location={0,0},const sf::Vector2f& velocity={0,0});
@@ -9,6 +12,8 @@ class Planet :public Celestial_body
 
 class Star :public Celestial_body
 {
+	std::string stellar_classification; //tak jak w przypadku planety; zgodnie z diagramem Hertzsprung–Russella będzie można  znieniać kolor czy teksture gwiazdy
+
 	public:
 	
 	Star(unsigned int mass=100, sf::Color& star_color=sf::Color::Yellow, sf::Vector2f& location={0,0},const sf::Vector2f& velocity={0,0});
