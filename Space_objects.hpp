2f30a313a69d53b798af0b36feaca8109c32a8e5
PanZWarzywniaka <tupolewcx@gmail.com>
2019-03-07 18:15:43 +0100

    Uzupełnianie klasy

--- Space_objects.hpp
+++ Space_objects.hpp
@@ -2,12 +2,14 @@
 
 class Planet :public Celestial_body
 {
+	public:
 	
 	Planet(unsigned int mass=10, sf::Color& planet_color=sf::Color::Blue, sf::Vector2f& location={0,0},const sf::Vector2f& velocity={0,0});
 };
 
 class Star :public Celestial_body
 {
+	public:
 	
 	Star(unsigned int mass=100, sf::Color& star_color=sf::Color::Yellow, sf::Vector2f& location={0,0},const sf::Vector2f& velocity={0,0});
 };
