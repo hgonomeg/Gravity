9a067a9c5d1a7dc282546d7e1716928ddbe9bf4b
PanZWarzywniaka <tupolewcx@gmail.com>
2019-03-07 18:51:27 +0100

    Propozycje dalszych zmian czytajkomentarze w kodzie

--- Celestial_body.hpp
+++ Celestial_body.hpp
@@ -2,11 +2,17 @@
 
 class Celestial_body :public sf::Drawable
 {
+	
+	
 	int mass;
 	sf::Vector2f loc;
 	sf::Vector2f v;
 	sf::CircleShape wyglond;
 	sf::Color thecol;
+	
+	float volume; //o to moja propozycje nie zmieniałem jeszcze konstruktora Celestial_body
+	float density;  // dzięki tym zmiennym będzie można na bierząco liczyć promień naszego obiektu 
+	
 	public:
 	
 	Celestial_body(int,const sf::Color& et=sf::Color::White,const sf::Vector2f& ye={0,0},const sf::Vector2f& ey={0,0}); 
