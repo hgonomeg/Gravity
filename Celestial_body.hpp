3870b81ee8b1c1fe11c3af3676102475cfbb4b22
PanZWarzywniaka <tupolewcx@gmail.com>
2019-03-08 22:14:56 +0100

    Stworzenie, funkcji w klasie Celestial body które zwracają, promień, odległość między obiektami i sprawdzają czy następuje kolizja

--- Celestial_body.hpp
+++ Celestial_body.hpp
@@ -1,6 +1,7 @@
 #ifndef CELESTIAL_BODY_HPP
 #define CELESTIAL_BODY_HPP
 #include <SFML/Graphics.hpp>
+#include <math.h>
 
 class Celestial_body :public sf::Drawable
 {
@@ -16,15 +17,22 @@ class Celestial_body :public sf::Drawable
 	float volume; //o to moja propozycje nie zmieniałem jeszcze konstruktora Celestial_body
 	float density;  // dzięki tym zmiennym będzie można na bierząco liczyć promień naszego obiektu 
 	
+	float radius;
+	
 	public:
 	
 	Celestial_body(int,const sf::Color& et=sf::Color::White,const sf::Vector2f& ye={0,0},const sf::Vector2f& ey={0,0}); 
 	
 	virtual void draw(sf::RenderTarget& tgt,sf::RenderStates st) const override; // "override" upewnienie się nadpisania metody z klasy od której dzidziczymy
 	int& get_mass();
+	float& get_radius();
 	void refresh();
 	sf::Vector2f& get_loc();
 	sf::Vector2f& get_v();
+	
+	
+	/*static?*/float distance_from(Celestial_body* CB1, Celestial_body* CB2); //liczenie odległości między dwoma obiektami, jako argumenty przyjmuje wskaźniki do obiektów
+	static bool collision_detec(Celestial_body* CB1, Celestial_body* CB2); //detekcja kolizji dwóch CB, jako argumenty przyjmuje wskaźniki do obiektów
 	
 	
 };
