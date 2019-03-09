f332c9f69cc740c1f64e98e11355595dd676c4b2
hgonomeg <hgonomeg@gmail.com>
2019-03-09 13:33:47 +0100

    stylistyczne popr.

--- Celestial_body.hpp
+++ Celestial_body.hpp
@@ -20,13 +20,13 @@ class Celestial_body :public sf::Drawable
 	
 	Celestial_body(int,const sf::Color& et=sf::Color::White,const sf::Vector2f& ye={0,0},const sf::Vector2f& ey={0,0}); 
 	
-	virtual void draw(sf::RenderTarget& tgt,sf::RenderStates st) const override; // "override" upewnienie się nadpisania metody z klasy od której dzidziczymy
+	virtual void draw(sf::RenderTarget& tgt,sf::RenderStates st) const override; // "override" upewnienie się nadpisania metody z klasy od której dziedziczymy
 	int& get_mass();
 	float& get_radius();
-	void refresh();
 	sf::Vector2f& get_loc();
 	sf::Vector2f& get_v();
 	
+	void refresh();
 	
 	static float distance_from(Celestial_body* CB1, Celestial_body* CB2); //liczenie odległości między dwoma obiektami, jako argumenty przyjmuje wskaźniki do obiektów
 	static bool collision_detec(Celestial_body* CB1, Celestial_body* CB2); //detekcja kolizji dwóch CB, jako argumenty przyjmuje wskaźniki do obiektów
