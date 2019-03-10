b22221e7e79c5b055f0e8461016c60d2623949dc
hgonomeg <hgonomeg@gmail.com>
2019-03-10 17:04:41 +0100

    zapomniałem dodać Sim_chrono

---
+++ Sim_chrono.cpp
@@ -0,0 +1,30 @@
+#include "Grawitacja.hpp"
+
+const std::string Sim_chrono::nam="Simulation pace manager";
+
+const std::string& Sim_chrono::name()
+{
+	return nam;
+}
+
+void Sim_chrono::mbp(sf::Event& ev)
+{
+	
+}
+void Sim_chrono::mbr(sf::Event& ev)
+{
+	
+}
+void Sim_chrono::kbp(sf::Event& ev)
+{
+	
+}
+void Sim_chrono::draw(sf::RenderTarget& tgt,sf::RenderStates st) const
+{
+	
+}
+
+Sim_chrono::Sim_chrono()
+{
+	
+}
