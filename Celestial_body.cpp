88f5e782ec1fabb0ce4416a15b415594a4b9e2a5
hgonomeg <hgonomeg@google.com>
2019-10-24 21:50:37 +0200

    Estetyka i kompilowalność

--- Celestial_body.cpp
+++ Celestial_body.cpp
@@ -297,16 +297,16 @@ void Celestial_body::bounce_handle(Celestial_body* matka, Celestial_body* ojciec
 
 	// 2. Wyznaczamy wektor r12 różnicy położeń tych ciał.
 
-	sf::Vector2f = poz_matki-poz_ojca;
+	sf::Vector2f vect_differentiam = poz_matki-poz_ojca;
 
 	// 3. Rzutujemy wektor v1 oraz wektor v2 na kierunek wektora r12 (można to zrobić bez użycia równania prostej, można wykorzystać własność iloczynu skalarnego, w ten sposób otrzymamy konkretne współrzędne wektora vC1 oraz vc2 (składowych prędkości wzdłuż prostej łączącej środki kul)).
 	
 	
-	
+	/*
 	4. Szukamy prostopadłych do tej prostej składowych wektorów v1 i v2, np. poprzez różnicę wektorową: vp1 = v1 - vC1 ; vp2 = v2-vC2.
 	5. Zderzenie sprężyste zachodzi tak, że składowe vC1 i vC2 przetransformują się wg wzorów opisujących zderzenie centralne (które znaleźliście), natomiast składowe vp1 i vp2 pozostaną bez zmian (gdyby kule miały jedynie składowe vp1 i vp2 (tzn v1 = vp1 oraz v2 = vp2, to kule jedynie "musnęłyby się" bez zmiany prędkości)).
 	6. Wektor prędkości po zderzeniu jest równy u1 = vp1 + uC1  ;  u2 = vp2 + uC2 ; gdzie uC1 i uC2 to przetransformowane wektory vC1 i vC2 wg wzorów opisujących zderzenie sprężyste centralne.
-
+	*/
 	
 }
 
