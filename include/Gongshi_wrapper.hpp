97413251b955b1c887d44ea1001b65d2833a4a0f
Olek <tupolewcx@gmail.com>
2020-04-18 23:27:40 +0200

    Copying Gongshi from Tianche

--- include/Gongshi_wrapper.hpp
+++ include/Gongshi_wrapper.hpp
@@ -14,7 +14,11 @@ template <typename T>
         using iter_type = typename std::list<T>::iterator;
         using fx_type = std::function<void(const iter_type&,const iter_type&)>;
 
-        
+        std::list<T>& subject_list;
+		std::condition_variable thread_sleeper;
+		std::vector<std::thread> thread_pool;
+		std::queue<std::pair<unsigned int,fx_type>> work_queue; //A queue that holds the tianche multipliers and the function to be applied to each unique pair of objects
+		std::mutex queue_mutex;
 
 
     };
